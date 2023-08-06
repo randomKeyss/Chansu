4930eee13dd81b69a55e9695e1fa7469b91aac34
Ram Potham <ram.potham@gmail.com>
2023-08-05 22:07:10 -0400

    actually working

---
+++ Chansu/ChansuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChansuApp.swift
+//  Chansu
+//
+//  Created by Ram on 8/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ChansuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
