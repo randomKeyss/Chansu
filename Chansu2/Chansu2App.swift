f117e541fd899c6af77868b45c1ddc50692f703b
randomKeyss <davidrwallach@gmail.com>
2023-08-05 20:26:13 -0500

    start project

---
+++ Chansu2/Chansu2App.swift
@@ -0,0 +1,17 @@
+//
+//  Chansu2App.swift
+//  Chansu2
+//
+//  Created by David Wallach on 8/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Chansu2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
